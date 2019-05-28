168d712b1c9ef10268b3603b544f106c6e1f33e0
Raffael Azevedo <raffael.azevedo@gmail.com>
2019-05-28 15:10:24 -0300

    Fixed resolution of Bhaskara's Formule using Python3

--- URI1036.py
+++ URI1036.py
@@ -6,12 +6,11 @@ a = float(a)
 b = float(b)
 c = float(c)
 
-delta = (b ** 2) - (4 * a * c)
-r1 = (-b+(math.sqrt(delta)))/(2*a)
-r2 = -b - (math.sqrt(delta))/(2*a)
-
-if(math.sqrt(delta) < 0 or float(a) == 0):
-    print("Impossivel calcular")
+delta = (b**2)-(4*a*c)
+if(delta < 0 or a == 0):
+	print("Impossivel calcular")
 else:
-    print("R1 = %.5f" %r1)
-    print("R2 = %5.f" %r2)
+	r1 = (-b+(math.sqrt(delta)))/(2*a)
+	r2 = (-b-(math.sqrt(delta)))/(2*a)
+	print("R1 = %.5f" %r1)
+	print("R2 = %.5f" %r2)
